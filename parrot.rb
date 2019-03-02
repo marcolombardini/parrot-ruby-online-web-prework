e7b940febe8742b06cc4fb6226f09f1c5a777a4e
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-02 19:07:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def #parrot(phrase = "Pretty Bird!")
+def parrot(phrase = "Pretty Bird!")
   puts "I am a #{phrase}",
   "Squawk!"
 end
